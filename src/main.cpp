3ddc6deb37aa0e454190d707e8b73631f09d0793
likangle <likangle@corp.netease.com>
2024-03-23 00:11:20 +0800

    feat: 树莓派pico测试

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,46 @@
 #include <Arduino.h>
+#include "hardware/flash.h"
+
+using namespace arduino;
+
+#define LED_PIN 25
+
+int count = 0;
 
 void setup()
 {
     Serial.begin(9600);
-    Serial.printf("Setup done: %d\n", 111);
+    Serial.println("Hello World");
+
+    pinMode(LED_PIN, OUTPUT);
 }
 
 void loop()
 {
-    Serial.println("1111");
+    void *m = malloc(40960);
+    if (m != nullptr)
+    {
+        Serial.println("Memory allocated ok.");
+        free(m);
+    }
+    else
+    {
+        Serial.println("Memory allocation failed");
+    }
+
+    Serial.println(String("--> Loop count: ") + String(count++));
+
+    for (int i = 0; i < 256; i++)
+    {
+        analogWrite(LED_PIN, i);
+        delay(1);
+    }
+
+    for (int i = 255; i >= 0; i--)
+    {
+        analogWrite(LED_PIN, i);
+        delay(1);
+    }
 
     delay(500);
 }
