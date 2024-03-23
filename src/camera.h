7d54ce400e76241ef2edfbca819f479a0f2f147d
likangle <likangle@corp.netease.com>
2024-03-23 16:24:22 +0800

    fix: rtos1

--- src/camera.h
+++ src/camera.h
@@ -48,6 +48,38 @@ static camera_config_t camera_config = {
     .frame_size = FRAMESIZE_240X240,
 
     // .jpeg_quality = 5,                 // 0-63 lower number means higher quality
+    .fb_count = 10,                    // if fb_count more than one, the driver will work in continuous mode.
+    .fb_location = CAMERA_FB_IN_PSRAM, // esp32没有psram，所以只能使用DRAM
+    .grab_mode = CAMERA_GRAB_LATEST    // CAMERA_GRAB_LATEST. Sets when buffers should be filled
+};
+
+static camera_config_t camera_config2 = {
+    .pin_pwdn = CAM_PIN_PWDN,
+    .pin_reset = CAM_PIN_RESET,
+    .pin_xclk = CAM_PIN_XCLK,
+    .pin_sccb_sda = CAM_PIN_SDA,
+    .pin_sccb_scl = CAM_PIN_SCL,
+
+    .pin_d7 = CAM_PIN_D7,
+    .pin_d6 = CAM_PIN_D6,
+    .pin_d5 = CAM_PIN_D5,
+    .pin_d4 = CAM_PIN_D4,
+    .pin_d3 = CAM_PIN_D3,
+    .pin_d2 = CAM_PIN_D2,
+    .pin_d1 = CAM_PIN_D1,
+    .pin_d0 = CAM_PIN_D0,
+    .pin_vsync = CAM_PIN_VSYNC,
+    .pin_href = CAM_PIN_HREF,
+    .pin_pclk = CAM_PIN_PCLK,
+
+    .xclk_freq_hz = 20000000,
+    .ledc_timer = LEDC_TIMER_0,
+    .ledc_channel = LEDC_CHANNEL_0,
+
+    .pixel_format = PIXFORMAT_RGB565, // YUV422,GRAYSCALE,RGB565,JPEG
+    .frame_size = FRAMESIZE_UXGA,
+
+    // .jpeg_quality = 5,                 // 0-63 lower number means higher quality
     .fb_count = 4,                     // if fb_count more than one, the driver will work in continuous mode.
     .fb_location = CAMERA_FB_IN_PSRAM, // esp32没有psram，所以只能使用DRAM
     .grab_mode = CAMERA_GRAB_LATEST    // CAMERA_GRAB_LATEST. Sets when buffers should be filled
