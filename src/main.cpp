ede4fc0d4b6e1ef2bf1a808af111418eb4538635
likangle <likangle@corp.netease.com>
2024-03-24 13:12:00 +0800

    fix: arduion_gfx

--- src/main.cpp
+++ src/main.cpp
@@ -1,93 +1,33 @@
 #include <Arduino.h>
-#include <camera.h>
-#include <lcd.h>
+#include <font/u8g2_font_unifont_t_chinese4.h>
+#include <Arduino_GFX_Library.h>
 
-xQueueHandle xqh = NULL;
-TaskHandle_t videoTask = NULL;
+#define BLO 2
 
-// 显示任务
-void display(void *p)
-{
-    LGFX lcd;
-    lcd.init();
-    lcd.setRotation(0);
-    lcd.setBrightness(200);
-    lcd.setColorDepth(16);
-    lcd.fillScreen(TFT_BLACK);
-
-    for (;;)
-    {
-        camera_fb_t fb;
-        if (xQueueReceive(xqh, &fb, 5) == pdTRUE)
-        {
-            lcd.pushImage(0, 0, fb.width, fb.height, (uint16_t *)fb.buf);
-            heap_caps_free(fb.buf);
-        }
-    }
-
-    vTaskDelete(NULL);
-}
-
-void video(void *p)
-{
-    if (esp_camera_init(&camera_config) != ESP_OK)
-    {
-        Serial.println("Camera Init Failed");
-        return;
-    }
-    Serial.println("Camera Init Succeed");
-
-    for (;;)
-    {
-        camera_fb_t *fb = esp_camera_fb_get();
-        if (!fb)
-        {
-            Serial.println("Camera Capture Failed");
-            return;
-        }
-
-        uint8_t *buf = (uint8_t *)heap_caps_malloc(fb->len * sizeof(uint8_t), MALLOC_CAP_DEFAULT);
-        if (buf != NULL)
-        {
-            memcpy(buf, fb->buf, fb->len);
-            camera_fb_t cfb = {
-                .buf = buf,
-                .len = fb->len,
-                .width = fb->width,
-                .height = fb->height,
-            };
-
-            if (xQueueSend(xqh, &cfb, 5) != pdPASS)
-            {
-                Serial.println("Queue Send Failed");
-            }
-        }
-
-        esp_camera_fb_return(fb);
-    }
-
-    vTaskDelete(NULL);
-}
+Arduino_DataBus *bus = new Arduino_HWSPI(26, 15, 14, 13, -1);
+Arduino_ST7789 gfx(bus, 27, 0, true, 240, 240);
 
 void setup()
 {
     Serial.begin(9600);
 
-    xqh = xQueueCreate(10, sizeof(camera_fb_t));
-    if (xqh == NULL)
-    {
-        Serial.println("queue create fail...");
-        return;
-    }
+    // 背光亮起来
+    pinMode(BLO, OUTPUT);
+    digitalWrite(BLO, HIGH);
 
-    if (xTaskCreatePinnedToCore(display, "display", 8192, NULL, 1, NULL, 0) != pdTRUE)
+    if (!gfx.begin())
     {
-        Serial.println("Task(display) Create Failed");
+        Serial.println("ST7789 failed!");
     }
-    if (xTaskCreatePinnedToCore(video, "video", 8192, NULL, 1, &videoTask, 0) != pdTRUE)
-    {
-        Serial.println("Task(video) Create Failed");
-    }
+    Serial.println("ST7789 initialization done!");
+
+    gfx.fillScreen(BLACK);
+
+    gfx.setFont(u8g2_font_unifont_t_chinese4);
+    gfx.setUTF8Print(true);
+    gfx.setTextColor(WHITE);
+
+    gfx.print("你好，世界！");
 }
 
 void loop()
